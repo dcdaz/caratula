0513d19613529626fe021f30b69a19da4af0bc4f
Daniel Cordova <danesc87@gmail.com>
2024-01-24 18:48:04 -0500

    Minor improvements on custom pages

--- src/components/CustomPages.vue
+++ src/components/CustomPages.vue
@@ -1,24 +1,25 @@
 <template>
     <div class="container">
             <div class="columns">
-                <div class="column is-2" />
+                <div v-if="customPages.length < 5" class="column is-2" />
                 <div class="tile is-ancestor">
                     <div class="tile is-parent">
                         <div v-for="customPage in customPages" :key="customPage" class="column tile">
                             <div class="tile is-child box">
-                                <p class="title is-4 has-text-centered has-text-weight-semibold is-capitalized">
-                                    <svg-icon id="middle-aligment" type="mdi" :path="allIcons[customPage.name]" />&nbsp;&nbsp;{{ customPage.name }}
+                                <p class="tag is-white title is-4 has-text-centered has-text-weight-semibold is-capitalized">
+                                    <svg-icon id="middle-aligment" type="mdi" :path="allIcons[customPage.icon]" />&nbsp;&nbsp;{{ customPage.name }}
                                 </p>
-                                <p v-for="link in customPage.links" :key="link" :class="column">
-                                    <a v-if="link.name != null" class="has-text-weight-medium is-capitalized has-text-grey-dark" :href="link.url">
-                                        <svg-icon id="middle-aligment" type="mdi" :path="allIcons[link.name]" :size="19" />&nbsp;&nbsp;{{ link.name }}
+                                <div v-for="link in customPage.links" :key="link">
+                                    <svg-icon id="middle-aligment" type="mdi" :path="allIcons[link.icon]" :size="19" />
+                                    <a v-if="link.name != null" class="tag is-white has-text-weight-medium is-capitalized has-text-grey-dark" :href="link.url">
+                                        {{ link.name }}
                                     </a>
-                                </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div class="column is-2" />
+                <div v-if="customPages.length < 5" class="column is-2" />
             </div>
         </div>
 </template>
@@ -39,18 +40,18 @@
             }
         },
         methods: {
-            async getIcon(name, iconName) {
+            async getIcon(iconName) {
                 const iconImported = { [iconName]: this.icon } = await import('@mdi/js')
-                this.allIcons[name] = this.icon;
+                this.allIcons[iconName] = this.icon;
                 this.icon = null
                 return iconImported;
             }
         },
         async created() {
             this.customPages.forEach(category => {
-                this.getIcon(category.name, category.icon);
+                this.getIcon(category.icon);
                 category.links.forEach(link =>{
-                    this.getIcon(link.name, link.icon);
+                    this.getIcon(link.icon);
                 });
             });
         },
